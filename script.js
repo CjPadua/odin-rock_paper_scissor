b9c34e6ffe3c828badb8ea4a5fddf7aba7fb3ac1
Cj Padua <cediejamesapadua@gmail.com>
2026-05-23 17:05:32 +0800

    Create a function to simulate random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,136 @@
-console.log("Hello World");
+/*
+FUNCTION getComputerChoice()
+	SET choice equal to blank
+
+	SET randomNumber equal to 0
+	GET a random number
+	SET randomNumber equal to the random number
+
+	IF randomNumber < 0.3 THEN
+		SET choice equal to rock
+	ELSE IF randomNumber < 0.6 THEN
+		SET choice equal to paper
+	ELSE
+		SET choice equal to scissor
+	END IF
+
+	RETURN choice
+END FUNCTION
+
+FUNCTION getHumanChoice()
+	SET choice equal to blank
+
+	DISPLAY Rock, Paper, or Scissor?
+	GET user input 
+SET choice equal to user input
+
+RETURN  choice
+END FUNCTION
+
+FUNCTION playRound()
+	SET humanChoice equal to blank
+	SET computerChoice equal to blank
+
+	CALL getHumanChoice RETURNING choice
+	SET humanChoice equal to choice
+	SET humanChoice equal to lowercase humanChoice
+
+	CALL getComputerChoice RETURNING choice
+	SET computerChoice equal to choice
+
+	IF humanChoice is equals computerChoice THEN
+		RETURN draw
+	END IF
+
+	CASE humanChoice OF
+		rock:
+			IF computerChoice equals paper THEN
+				RETURN computer
+			ELSE
+				RETURN human
+			END IF
+		paper:
+			IF computerChoice equals scissor THEN
+				RETURN computer
+			ELSE
+				RETURN human
+			END IF			
+		scissor:
+			IF computerChoice equals rock THEN
+				RETURN computer
+			ELSE
+				RETURN human
+			END IF
+	END CASE
+END FUNCTION
+
+FUNCTION playGame()
+	SET humanScore equal to 0
+	SET computerScore equal to 0
+
+	SET rounds equal to 0
+
+	WHILE rounds is less than 5  
+		CALL playRound RETURNING winner
+
+		CASE winner OF 
+			human:
+				INCREMENT humanScore
+computer: 
+	INCREMENT computerScore
+END CASE			
+
+		INCREMENT rounds
+	END WHILE
+
+	SET winner equal to blank
+	
+IF humanScore is equals to computerScore THEN
+	SET winner equal to none
+ELSE IF humanScore is greater than computerScore THEN
+	SET winner equal to human
+ELSE 
+	SET winner equal to computer	
+END IF
+
+DISPLAY winner
+END FUNCTION
+*/
+
+/*
+FUNCTION getComputerChoice()
+	SET choice equal to blank
+
+	SET randomNumber equal to 0
+	GET a random number
+	SET randomNumber equal to the random number
+
+	IF randomNumber < 0.3 THEN
+		SET choice equal to rock
+	ELSE IF randomNumber < 0.6 THEN
+		SET choice equal to paper
+	ELSE
+		SET choice equal to scissor
+	END IF
+
+	RETURN choice
+END FUNCTION
+*/
+
+function getComputerChoice() {
+  let choice;
+
+  const randomNumber = Math.random();
+
+  if (randomNumber < 0.3) {
+    choice = 'rock';
+  }
+  else if (randomNumber < 0.6) {
+    choice = 'paper';
+  }
+  else {
+    choice = 'scissor';
+  }
+
+  return choice;
+}
